dddcd03bba6b2a4de659d93de1d8bc81c40a3092
leticia.takenaka <leticia.takenaka@BHN050103221>
2023-11-20 14:31:57 -0300

    03: Exibindo as músicas de um álbum

--- Musica/Album.cs
+++ Musica/Album.cs
@@ -2,14 +2,21 @@
 using System;
 using System.Runtime.CompilerServices;
 
-class Album(List<Musica> musicas, string tituloAlbum, int duracaoTotalAlbum)
+class Album(List<Musica> musicas, string tituloAlbum)
 {
     private List<Musica> musicas = musicas;
     public string TituloAlbum { get; set; } = tituloAlbum;
-    public int DuracaoTotalAlbum { get; set; } = duracaoTotalAlbum;
+    public int DuracaoTotalAlbum => musicas.Sum(musica => musica.DuracaoMusica);
 
     public void AdicionarMusicaAoAlbum(Musica musica)
     {
         musicas.Add(musica);
+    }
+
+    public void ExibirMusicasDoAlbum()
+    {
+        Console.WriteLine($"Álbum: {TituloAlbum} - Duração do álbum com as músicas selecioandas: {DuracaoTotalAlbum}s");
+        musicas.ForEach(musica => Console.WriteLine($"Música: {musica.TituloMusica} - {musica.DuracaoMusica}s"));
+        Console.WriteLine("\n");
     }
 }
--- Musica/Program.cs
+++ Musica/Program.cs
@@ -1,6 +1,12 @@
 ﻿using Alura_POO;
 
-Musica musicaDoAlbum1 = new("Love of my life", "Queen", 300, true);
-Musica musicaDoAlbum2 = new("Bohemian Raphsody", "Queen", 300, true);
+Musica musica1 = new("Cool Cat", "Queen", 196, true);
+Musica musica2 = new("Spread Your Wings", "Queen", 259, true);
+Album album1 = new([musica1,musica2], "News of the World");
 
-Album albumDoQueen = new([musicaDoAlbum1, musicaDoAlbum2], "A night at the opera", 4000);
+Musica musica3 = new("Sleeping On the Sidewalk", "Queen", 185, true);
+Album album2 = new([musica3], "Hot Space");
+
+
+album1.ExibirMusicasDoAlbum();
+album2.ExibirMusicasDoAlbum();
