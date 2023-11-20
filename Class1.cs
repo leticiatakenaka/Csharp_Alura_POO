7a1a2e7ec9c274f078137c633801685450f0a9af
leticia.takenaka <leticia.takenaka@BHN050103221>
2023-11-20 08:25:28 -0300

    Aula 3: Criando a classe álbum

---
+++ Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Album
+{
+    public string TituloAlbum { get; set; }
+    public int DuracaoTotal { get; set; }
+    public void AdicionarMusica(Musica musica)
+    {
+
+    }
+}
